c551d222ed390e5dcfe368256283bcd1aa35db24
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-02-29 11:08:44 +0100

    Created models for database, updated .sql a bit

--- models.py
+++ models.py
@@ -105,15 +105,113 @@ class Ticket(Model):
         on_update=fields.CASCADE,
     )
     price = fields.FloatField(null=False)
-    
-    
+
+
 class Act(Model):
     actId = fields.IntField(pk=True)
-    title = fields.CharField(max_length=255, null=False)
-    duration = fields.IntField(null=False)
+    number = fields.IntField(null=False)
+    name = fields.CharField(max_length=255, null=False)
     playId = fields.ForeignKeyField(
         "models.Play",
         related_name="acts",
         on_delete=fields.CASCADE,
         on_update=fields.CASCADE,
     )
+
+    class Meta:
+        unique_together = ("number", "playId")
+
+
+class Role(Model):
+    roleId = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255, null=False)
+
+
+class Actor(Model):
+    actorId = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255, null=False)
+    email = fields.CharField(max_length=255, null=False)
+    status = fields.BooleanField(null=False)
+
+
+class Employee(Model):
+    employeeId = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255, null=False)
+    email = fields.CharField(max_length=255, null=False)
+    status = fields.BooleanField(null=False)
+
+
+class ActorRole(Model):
+    actorRoleId = fields.IntField(pk=True)
+    actorId = fields.ForeignKeyField(
+        "models.Actor",
+        related_name="roles",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+    roleId = fields.ForeignKeyField(
+        "models.Role",
+        related_name="actors",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+
+    class Meta:
+        unique_together = ("actorId", "roleId")
+
+
+class ActorPlay(Model):
+    actorPlayId = fields.IntField(pk=True)
+    actorId = fields.ForeignKeyField(
+        "models.Actor",
+        related_name="plays",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+    playId = fields.ForeignKeyField(
+        "models.Play",
+        related_name="actors",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+
+    class Meta:
+        unique_together = ("actorId", "playId")
+
+
+class EmployeePlay(Model):
+    employeePlayId = fields.IntField(pk=True)
+    employeeId = fields.ForeignKeyField(
+        "models.Employee",
+        related_name="plays",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+    playId = fields.ForeignKeyField(
+        "models.Play",
+        related_name="employees",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+
+    class Meta:
+        unique_together = ("employeeId", "playId")
+
+
+class ActRole(Model):
+    actRoleId = fields.IntField(pk=True)
+    roleId = fields.ForeignKeyField(
+        "models.Role",
+        related_name="acts",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+    actId = fields.ForeignKeyField(
+        "models.Act",
+        related_name="roles",
+        on_delete=fields.CASCADE,
+        on_update=fields.CASCADE,
+    )
+
+    class Meta:
+        unique_together = ("roleId", "actId")
