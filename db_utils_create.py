444ad38a10b699dfdf0c1e24c699c9a267c06095
Ivareh <69577035+Ivareh@users.noreply.github.com>
2024-03-12 10:05:20 +0100

    Fixed utils create with new database setup

--- db_utils_create.py
+++ db_utils_create.py
@@ -16,15 +16,6 @@ def create_connection(db_file):
     return conn
 
 
-def create_season(conn, season):
-    sql = """ INSERT INTO season(season, year)
-              VALUES(?,?) """
-    cur = conn.cursor()
-    cur.execute(sql, season)
-    conn.commit()
-    return cur.lastrowid
-
-
 def create_theater_hall(conn, theater_hall):
     sql = """ INSERT INTO theaterHall(name, capacity)
               VALUES(?,?) """
@@ -34,20 +25,20 @@ def create_theater_hall(conn, theater_hall):
     return cur.lastrowid
 
 
+def create_season(conn, season):
+    sql = """ INSERT INTO season(season, year)
+              VALUES(?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, season)
+    conn.commit()
+    return cur.lastrowid
+
+
 def create_play(conn, play):
     sql = """ INSERT INTO play(title, author, duration, hall_id, season_id)
               VALUES(?,?,?,?,?) """
     cur = conn.cursor()
     cur.execute(sql, play)
-    conn.commit()
-    return cur.lastrowid
-
-
-def create_act(conn, act):
-    sql = """ INSERT INTO act(number, name, play_id)
-              VALUES(?,?,?) """
-    cur = conn.cursor()
-    cur.execute(sql, act)
     conn.commit()
     return cur.lastrowid
 
@@ -70,6 +61,33 @@ def create_area(conn, area):
     return cur.lastrowid
 
 
+def create_chair(conn, chair):
+    sql = """ INSERT INTO chair(number, row, area_id, hall_id)
+              VALUES(?,?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, chair)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_chair_with_id(conn, chair):
+    sql = """ INSERT INTO chair(chair_id, number, row, area_id, hall_id)
+              VALUES(?,?,?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, chair)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_customer_profile(conn, customer_profile):
+    sql = """ INSERT INTO customerProfile(name, address, phone)
+              VALUES(?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, customer_profile)
+    conn.commit()
+    return cur.lastrowid
+
+
 def create_customer_group(conn, customer_group):
     sql = """ INSERT INTO customerGroup(name)
               VALUES(?) """
@@ -79,19 +97,11 @@ def create_customer_group(conn, customer_group):
     return cur.lastrowid
 
 
-def create_chair(conn, chair):
-    sql = """ INSERT INTO chair(number, row, area_id, hall_id)
-              VALUES(?,?,?,?) """
+def create_ticket_purchase(conn, ticket_purchase):
+    sql = """ INSERT INTO ticketPurchase(datetime, customer_id)
+              VALUES(?,?) """
     cur = conn.cursor()
-    cur.execute(sql, chair)
-    conn.commit()
-    return cur.lastrowid
-
-def create_chair_with_id(conn, chair):
-    sql = """ INSERT INTO chair(chair_id, number, row, area_id, hall_id)
-              VALUES(?,?,?,?,?) """
-    cur = conn.cursor()
-    cur.execute(sql, chair)
+    cur.execute(sql, ticket_purchase)
     conn.commit()
     return cur.lastrowid
 
@@ -123,28 +133,73 @@ def create_ticket_with_id(conn, ticket):
     return cur.lastrowid
 
 
-def create_ticket_chair(conn, ticket_chair):
-    sql = """ INSERT INTO ticketChair(chair_id, ticket_id)
-              VALUES(?,?) """
-    cur = conn.cursor()
-    cur.execute(sql, ticket_chair)
-    conn.commit()
-    return cur.lastrowid
-
-
-def create_customer_profile(conn, customer_profile):
-    sql = """ INSERT INTO customerProfile(name, address, phone)
+def create_act(conn, act):
+    sql = """ INSERT INTO act(number, name, play_id)
               VALUES(?,?,?) """
     cur = conn.cursor()
-    cur.execute(sql, customer_profile)
+    cur.execute(sql, act)
     conn.commit()
     return cur.lastrowid
 
 
-def create_ticket_purchase(conn, ticket_purchase):
-    sql = """ INSERT INTO ticketPurchase(datetime, customer_id)
+def create_role(conn, act):
+    sql = """ INSERT INTO role(name, play_id)
               VALUES(?,?) """
     cur = conn.cursor()
-    cur.execute(sql, ticket_purchase)
+    cur.execute(sql, act)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_actor(conn, actor):
+    sql = """ INSERT INTO actor(name, email, status, description)
+              VALUES(?,?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, actor)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_employee(conn, employee):
+    sql = """ INSERT INTO employee(name, email, status, description, task)
+              VALUES(?,?,?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, employee)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_actor_role(conn, actor_role):
+    sql = """ INSERT INTO actorRole(actor_id, role_id)
+              VALUES(?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, actor_role)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_actor_play(conn, actor_play):
+    sql = """ INSERT INTO actorPlay(actor_id, play_id)
+              VALUES(?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, actor_play)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_employee_play(conn, employee_play):
+    sql = """ INSERT INTO employeePlay(employee_id, play_id)
+              VALUES(?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, employee_play)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_act_role(conn, act_role):
+    sql = """ INSERT INTO actRole(act_id, role_id)
+              VALUES(?,?) """
+    cur = conn.cursor()
+    cur.execute(sql, act_role)
     conn.commit()
     return cur.lastrowid
